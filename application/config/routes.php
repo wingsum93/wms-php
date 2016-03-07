46b2a60b5202fca933e8f7b2e251bf9882c4cdf4
Eric Ho <wingsum93@outlook.com>
2016-03-07 15:00:02 +0800

    add news insertion
    add db model method, route and form.

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,6 +50,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 
+
+$route['news/create'] = 'news/create';
 $route['news/(:any)'] = 'news/view/$1';
 $route['news'] = 'news';
 $route['(:any)'] = 'pages/view/$1';
