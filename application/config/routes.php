026d382679ad8eb2e5dcd2361d135a0b63babbe9
Eric Ho <wingsum93@outlook.com>
2016-03-07 17:44:44 +0800

    change route.

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,9 +52,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 
 $route['news/create'] = 'news/create';
-$route['news/(:any)'] = 'news/view/$1';
 $route['news'] = 'news';
-$route['(:any)'] = 'pages/view/$1';
 $route['default_controller'] = 'pages/view';
 $route['404_override'] = 'errors/page_missing';
 $route['translate_uri_dashes'] = FALSE;
+$route['news/(:any)'] = 'news/view/$1';
+$route['(:any)'] = 'pages/view/$1';
